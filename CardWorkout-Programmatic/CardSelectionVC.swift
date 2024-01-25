02ef621443dde6ac2f70095f5b464d750c44e489
Illia Kornyk <krekerbrow@gmail.com>
2024-01-25 20:04:23 +0200

    project setup

--- CardWorkout-Programmatic/CardSelectionVC.swift
+++ CardWorkout-Programmatic/CardSelectionVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CardSelectionVC.swift
 //  CardWorkout-Programmatic
 //
 //  Created by РОМ on 25.01.2024.
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CardSelectionVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .brown;
     }
+    
 
-
+    
 }
-
